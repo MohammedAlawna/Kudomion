dccfce714118e3a3032ff0b2574809d7803dd176
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-01-14 03:34:51 +0000

    Project Imported..
    - Imported the project files.. to the Git Control.

---
+++ Kudomion/Kudomion/DeckItem.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kudomion
+{
+    public class DeckItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+        public string thumbSrc { get; set; }
+        public string ydkeCode { get; set; }
+        
+    }
+}
