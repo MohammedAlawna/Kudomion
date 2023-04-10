c2e4897e02fa47eed28bebf3e4ea75165f5c9341
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-04-10 14:30:38 +0100

    UserUpdate..
    - User update page and code now working very well!! :D

--- Kudomion/Kudomion/UserSettings.xaml.cs
+++ Kudomion/Kudomion/UserSettings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kudomion.FirebaseManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +10,46 @@ using Xamarin.Forms.Xaml;
 
 namespace Kudomion
 {
+    
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    
     public partial class UserSettings : ContentPage
     {
+        FirebaseHelper firebase = new FirebaseHelper();
         public UserSettings()
         {
             InitializeComponent();
+        }
+
+        private async void SubmitButton(object sender, EventArgs e)
+        {
+            User getCurrentUser = await FirebaseHelper.GetUsrFromName(LoginPage.currentLoggedInUser);
+
+            //Assign New Values To The Current Logged In User.
+            User userToBeUpdated = new User {
+            name = nUser.Text,
+            password = nPass.Text,
+            points = getCurrentUser.points,
+            posts = getCurrentUser.posts,
+            duels = getCurrentUser.duels,
+            usertype = getCurrentUser.usertype,
+            //TODO Email.
+            //TODO ImageSrc.
+            };
+
+            //Update User Via Firebase.
+            await firebase.UpdateUser(LoginPage.currentLoggedInUser, userToBeUpdated);
+
+            //Reset Values
+            nUser.Text = String.Empty;
+            nEmail.Text = String.Empty;
+            nPass.Text = String.Empty;
+            nPassConfirm.Text = String.Empty;
+
+            //Display Alert
+            await DisplayAlert("User Updated!", "User Information changed successfully.", "OK!");
+
+            return;
         }
     }
 }
