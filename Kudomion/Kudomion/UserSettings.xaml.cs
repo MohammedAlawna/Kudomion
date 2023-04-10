d8db195e946d4c2f78898929ba0abed90f1962be
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-04-10 22:54:02 +0100

    Fixed Update User.
    - Fixed null values when you update the values. :D

--- Kudomion/Kudomion/UserSettings.xaml.cs
+++ Kudomion/Kudomion/UserSettings.xaml.cs
@@ -23,6 +23,10 @@ namespace Kudomion
 
         private async void SubmitButton(object sender, EventArgs e)
         {
+            try
+            {
+                if (AreEntriesEmpty() == true) return;
+            
             User getCurrentUser = await FirebaseHelper.GetUsrFromName(LoginPage.currentLoggedInUser);
 
             //Assign New Values To The Current Logged In User.
@@ -49,7 +53,41 @@ namespace Kudomion
             //Display Alert
             await DisplayAlert("User Updated!", "User Information changed successfully.", "OK!");
 
-            return;
+            
+            }
+            catch(NullReferenceException er)
+            {
+                await DisplayAlert("Error!", $"An Error Occured.. Missing Info..{er}", "OK!");
+            }
+        }
+
+        bool AreEntriesEmpty()
+        {
+            if (string.IsNullOrEmpty(nUser.Text))
+            {
+                DisplayAlert("Missing Credentials!", "Please Enter The New Username..", "OK!");
+                return true;
+            }
+
+            if(string.IsNullOrEmpty(nPass.Text))
+            {
+                DisplayAlert("Missing Credentials!", "Please Enter The New Password..", "OK!");
+                return true;
+            }
+
+            if(string.IsNullOrEmpty(nPassConfirm.Text))
+            {
+                DisplayAlert("Missing Credentials!", "Please Enter The New Password Confirmation..", "OK!");
+                return true;
+            }
+
+            return false;
+            /* TODO:: When Email Authentication Implemented In The Project.
+            if(nEmail.Text == "")
+            {
+                DisplayAlert("Missing Credentials!", "Please Enter The New Email..", "OK!");
+                return;
+            }*/
         }
     }
 }
