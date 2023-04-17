69cfb1c954a840834ca662edfb3ebee2b35fda7b
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-04-17 16:04:42 +0100

    Picker Functioning Simply!
    - The picker is now working as intended, with simple words.
    
    - When you change the picker item, it's going to change the view below. Now it's  just showing an alert, Gotta to code a new views below that get different data bindings and sources for the code. o3o

--- Kudomion/Kudomion/ViewModel/UserProfile.xaml.cs
+++ Kudomion/Kudomion/ViewModel/UserProfile.xaml.cs
@@ -19,8 +19,6 @@ namespace Kudomion.ViewModel
 
             //Load Picker Items.
             LoadPickerItems();
-
-
         }
 
         void LoadPickerItems()
@@ -29,6 +27,36 @@ namespace Kudomion.ViewModel
             userStatPicker.Items.Add("Duels");
             userStatPicker.Items.Add("Posts");
             userStatPicker.Items.Add("Signature");
+
+            //Set To First One.
+            userStatPicker.SelectedIndex = 0;
+
+            userStatPicker.SelectedIndexChanged += (sender, args) =>
+            {
+                if(userStatPicker.SelectedIndex == 0)
+                {
+                    //TODO Show user Profile Stats.
+                    DisplayAlert("Alert!", "You are vieweing user Stats!", "OK!");
+                }
+
+                if(userStatPicker.SelectedIndex == 1)
+                {
+                    //TODO Show user Duels.
+                    DisplayAlert("Alert!", "You are vieweing user Duels!", "OK!");
+                }
+
+                if(userStatPicker.SelectedIndex == 2)
+                {
+                    //TODO Show user Posts.
+                    DisplayAlert("Alert!", "You are viewing user Posts!", "OK!");
+                }
+
+                if(userStatPicker.SelectedIndex == 3)
+                {
+                    //TODO Show user Signature.
+                    DisplayAlert("Alert!", "You are viewing user Signature", "OK!");
+                }
+            };
         }
 
         private void InviteToDuelClicked(object sender, EventArgs e)
