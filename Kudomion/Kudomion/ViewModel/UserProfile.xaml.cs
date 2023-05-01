f50dfc89ff3ca080475cd4d5e693383aa779e7d2
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-05-01 14:21:11 +0100

    Styling The Profile Viewer! ~

--- Kudomion/Kudomion/ViewModel/UserProfile.xaml.cs
+++ Kudomion/Kudomion/ViewModel/UserProfile.xaml.cs
@@ -57,7 +57,7 @@ namespace Kudomion.ViewModel
 
             if(roomsForSpecifiedUser.Count > 0)
             {
-                await DisplayAlert("Duels List!", "Username: " + roomsForSpecifiedUser[0].p1 + " , " + roomsForSpecifiedUser[0].p2,"OK!");
+                //await DisplayAlert("Duels List!", "Username: " + roomsForSpecifiedUser[0].p1 + " , " + roomsForSpecifiedUser[0].p2,"OK!");
                 userDuels.ItemsSource = roomsForSpecifiedUser;
             }
            
