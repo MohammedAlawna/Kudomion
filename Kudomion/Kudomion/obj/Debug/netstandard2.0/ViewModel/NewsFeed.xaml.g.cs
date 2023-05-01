6914c58e7dbe790081f38e1a23db5df307428e40
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-05-01 14:51:36 +0100

    Main Structure for Posts System is there!
    o3o

--- Kudomion/Kudomion/obj/Debug/netstandard2.0/ViewModel/NewsFeed.xaml.g.cs
+++ Kudomion/Kudomion/obj/Debug/netstandard2.0/ViewModel/NewsFeed.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Kudomion.ViewModel {
     public partial class NewsFeed : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.CollectionView postsInNewsFeed;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(NewsFeed));
+            postsInNewsFeed = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CollectionView>(this, "postsInNewsFeed");
         }
     }
 }
