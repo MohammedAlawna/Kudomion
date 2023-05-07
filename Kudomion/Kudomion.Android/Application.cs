0a69c2d0dfe853b2af3795e88cfedd626c821c61
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-05-07 22:09:42 +0100

    5

--- Kudomion/Kudomion.Android/Application.cs
+++ Kudomion/Kudomion.Android/Application.cs
@@ -8,10 +8,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Plugin.FirebasePushNotification;
 
 namespace Kudomion.Droid
 {
-    class Application
+    [Application]
+    public class MainApplication : Application
     {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            //Set the default notification channel for your app when running Android Oreo
+            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+            {
+                FirebasePush
+                //Change for your default notification channel id here
+                FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
+
+                //Change for your default notification channel name here
+                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+            }
+
+            //If debug you should reset the token each time.
+#if DEBUG
+            FirebasePushNotificationManager.Initialize(this, true);
+#else
+              FirebasePushNotificationManager.Initialize(this,false);
+#endif
+
+            //Handle notification when app is closed here
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+            };
+        }
     }
 }
