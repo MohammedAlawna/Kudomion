8b7e1c1e56e0c72132cb58942ae0b90ddb94ced2
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2023-05-08 14:02:10 +0100

    Importing FCM Dependencies.. D:

--- Kudomion/Kudomion.Android/Application.cs
+++ Kudomion/Kudomion.Android/Application.cs
@@ -12,39 +12,8 @@ using Plugin.FirebasePushNotification;
 
 namespace Kudomion.Droid
 {
-    [Application]
-    public class MainApplication : Application
+   class Application
     {
-        public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
-        {
-        }
 
-        public override void OnCreate()
-        {
-            base.OnCreate();
-
-            //Set the default notification channel for your app when running Android Oreo
-            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
-            {
-                FirebasePush
-                //Change for your default notification channel id here
-                FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
-
-                //Change for your default notification channel name here
-                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
-            }
-
-            //If debug you should reset the token each time.
-#if DEBUG
-            FirebasePushNotificationManager.Initialize(this, true);
-#else
-              FirebasePushNotificationManager.Initialize(this,false);
-#endif
-
-            //Handle notification when app is closed here
-            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
-            {
-            };
-        }
     }
 }
